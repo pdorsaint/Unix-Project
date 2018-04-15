89505042c92dbd84895347cd5a37604d7c770114
pdorsaint <38406747+pdorsaint@users.noreply.github.com>
2018-04-15 18:29:06 -0400

    Create guessinggame.sh

---
+++ guessinggame.sh
@@ -0,0 +1,22 @@
+vi guessinggame.sh
+#! /bin/bash
+
+function hmfcd {
+echo "How many files are in the current directory?"
+}
+for number in {1..10}
+do
+if [[ $numner -lt 3 ]]
+do 
+echo "Incorrect, guess again"
+
+if [[ $number -gt 5 ]]
+do
+echo "Incorrect, try again"
+
+if [[ $number -eq 4 ]]
+do 
+echo "Congratulations, you are correct!"
+done
+done
+done
